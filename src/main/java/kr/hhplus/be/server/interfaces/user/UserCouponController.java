f9bf69f87b43693d8d8776a482bff843af1bb770
JayeeHong <hjy48486@naver.com>
2025-05-29 23:11:54 +0900

    fix: 선착순 쿠폰 발급 컨트롤러 수정

--- src/main/java/kr/hhplus/be/server/interfaces/user/UserCouponController.java
+++ src/main/java/kr/hhplus/be/server/interfaces/user/UserCouponController.java
@@ -4,8 +4,11 @@ import kr.hhplus.be.server.application.user.UserCouponFacade;
 import kr.hhplus.be.server.application.user.UserCouponResult;
 import kr.hhplus.be.server.interfaces.user.UserCouponRequest.Publish;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +21,7 @@ public class UserCouponController implements UserCouponApi {
 
     @Override
     @GetMapping("/{id}/coupons")
-    public UserCouponResponse.Coupons getCoupons(Long id) {
+    public UserCouponResponse.Coupons getCoupons(@PathVariable Long id) {
         UserCouponResult.Coupons userCoupons = userCouponFacade.getUserCoupons(id);
         return UserCouponResponse.Coupons.of(userCoupons);
     }
@@ -27,5 +30,7 @@ public class UserCouponController implements UserCouponApi {
     @PostMapping("/{id}/coupons/publish")
     public void chargeBalance(Long userId, Publish request) {
         userCouponFacade.issueUserCoupon(request.toCriteria(userId));
+    public ResponseEntity<Void> publishUserCoupon(@PathVariable("id") Long userId, @RequestBody Publish request) {
+        return ResponseEntity.accepted().build(); // 비동기라 Accepted 처리
     }
 }
